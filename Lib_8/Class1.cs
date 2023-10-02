e87fc374766f689623fb3c2b940f217817002b63
DonJuan <-31@LAB25-02>
2023-10-02 12:16:37 +0300

    Обновление 1.0

---
+++ Lib_8/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lib_8
+{
+    public class Class1 //БОЛОТО
+    {
+        static public int GetMax(int n, out List<int> generated)
+        {
+            generated = new List<int>(n);
+            int max = -1;
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int Chislo = rnd.Next(0, n + 1);
+                if ( Chislo > max )
+                {
+                    max = Chislo;
+                }
+                generated.Add(Chislo);
+            }
+            return max;
+
+        }    
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Работу выполнил студент группы ИСП-31 - Сазонов Иван Александрович \n" +
+                "Найти максимум из n целых случайных чисел X, распределенных в диапазоне от 0 \n" +
+                "до n. Вывести на экран на одной строке сгенерированные числа, на другой строке \n" +
+                "результат.");
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Enter(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(Input.Text, out int n))
+            {
+                if (n <= 0)
+                {
+                    MessageBox.Show("НЕ НАДО ДЯДЯ");
+                    return;
+                }
+                Results.Text = Lib_8.Class1.GetMax(n, out List<int> generated).ToString();
+                
+                Generate.Text = string.Join(';', generated);
+            }
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            Results.Clear();
+            Generate.Clear();
+        }
+    }
+}
